0d8dc52bf9933f01231b2e7e0f1a9cc18506ea5a
Dino Fernando <dinofernando@Dinos-MacBook-Pro.local>
2022-09-24 20:05:54 +0530

    console app successful

---
+++ GitLab_ProjectsAPI/Program.cs
@@ -0,0 +1,29 @@
+﻿using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text.Json;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+using System.Text.Json.Serialization;
+using GitLab_ProjectsAPI;
+
+
+
+
+var projects = await ProcessProjects();
+
+foreach (var project in projects)
+    Console.WriteLine(project.Name);
+
+static async Task<List<Project>> ProcessProjects()
+{
+    HttpClient client = new HttpClient();
+
+    var streamTask = client.GetStreamAsync("https://gitlab.com/api/v4/users/12624965/projects");
+    var projects = await JsonSerializer.DeserializeAsync<List<Project>>(await streamTask);
+    return projects;
+}
+
+
+
+
+
---
+++ GitLab_ProjectsAPI/project.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Diagnostics.CodeAnalysis;
+using System.Text.Json.Serialization;
+
+namespace GitLab_ProjectsAPI
+{
+    public class Project
+    {
+        [JsonPropertyName("name")]
+        public string Name { get; set; } = null!;
+
+    }
+    
+}
+
+
+
